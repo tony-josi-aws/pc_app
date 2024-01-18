f9d977b707a03dd8a8cf79b4242e841b0af1bac2
tony-josi-aws <tonyjosi@amazon.com>
2024-01-18 12:24:47 +0530

    Add main window icon

--- src/gui.py
+++ src/gui.py
@@ -1,6 +1,8 @@
-
+import os
 import sys
+import ctypes
 import logging
+import ui.resources.icons
 from ui.main_window import Ui_MainWindow
 from ui.ui_backend import PC_App_Handler
 from PyQt5 import QtCore, QtGui, QtWidgets
@@ -13,9 +15,20 @@ class PC_APP(QMainWindow):
         QMainWindow.__init__(self)
         super(PC_APP, self).__init__()
 
+        if os.name == 'nt':
+            '''
+            Fix main window icon display issue on windows due to peculiarities 
+            in how taskbar icons are handled on the Windows platform.
+
+            Refer: https://stackoverflow.com/q/1551605/6792356
+            '''
+            myappid = 'FreeRTOS.X-RayForFreeRTOS.2.3' # arbitrary string
+            ctypes.windll.shell32.SetCurrentProcessExplicitAppUserModelID(myappid)
+
         """ Set up the main window user interface. """
         self.mw_ui = Ui_MainWindow()
         self.mw_ui.setupUi(self)
+        self.setWindowIcon(QtGui.QIcon(':/icons/window_icon.jpg'))
 
         self.app_handle = PC_App_Handler(self.mw_ui)
 
